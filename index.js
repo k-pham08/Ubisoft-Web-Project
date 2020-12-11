ebc7b459e90d9c1ac0594b1b6e819dd49f229115
Khoa Pham <j.dangkhoa.118@gmail.com>
2020-12-11 15:42:35 +0700

    Finish Dropdown

---
+++ index.js
@@ -0,0 +1,7 @@
+function langClick(){
+    document.getElementById('lang-button').classList.toggle('active-lang');
+    document.getElementById('vn-button').classList.toggle('langdown');
+}
+function searchClick(){
+    document.querySelector("#input-search").classList.toggle("search-active");
+}
