c91bd4238a3f453a82ecbdbc13f544ff1bbda22c
Khoa Pham <j.dangkhoa.118@gmail.com>
2020-12-23 14:44:11 +0700

    add Store & Product Detail

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+
 function langClick(){
     document.getElementById('lang-button').classList.toggle('active-lang');
     document.getElementById('vn-button').classList.toggle('langdown');
 }
 function searchClick(){
-    document.querySelector("#input-search").classList.toggle("search-active");
+    document.querySelector('#input-search').classList.toggle('search-active');
 }
+
 function preloadInit(){
     const preload = document.querySelector(".preloader");
     const body = document.querySelector("html");
